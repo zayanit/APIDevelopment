b5e4a298116536b2fc2534f1a9b95c1e066b26a0
Mohammed Zayan <zayanit@gmail.com>
2021-07-07 21:59:26 +0300

    Add comments

--- flask_setup_example/flasker/__init__.py
+++ flask_setup_example/flasker/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify
 
 def create_app(test_config=None):
+  # Create and configure the app
   app = Flask(__name__)
 
+  # a simple page that says hello
   @app.route('/')
   def hello():
     return jsonify({'message': 'Hello world!'})
