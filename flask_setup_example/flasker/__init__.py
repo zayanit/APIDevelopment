7daed57e62a4d5abd3ed01abf7ee91d59db42155
Mohammed Zayan <zayanit@gmail.com>
2021-07-14 10:37:44 +0300

    Implement CORS
    Lesson 3
    Endpoints and Payloads
    4. Flask-CORS

--- flask_setup_example/flasker/__init__.py
+++ flask_setup_example/flasker/__init__.py
@@ -1,11 +1,23 @@
 from flask import Flask, jsonify
+from models import setup_db, Plant
+from flask_cors import CORS
 
 def create_app(test_config=None):
   # Create and configure the app
   app = Flask(__name__)
+  setup_db(app)
+  # CORS(app, resources={r"*/api/*" : {origins: '*'}})
+  CORS(app)
+
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authoraization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+    return response
 
   # a simple page that says hello
   @app.route('/')
+  # @cross_origin
   def hello():
     return jsonify({'message': 'Hello world!'})
 
